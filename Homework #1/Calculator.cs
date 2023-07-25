35ffa4e10eeb1c59fc179cd6a05cf6eaae09881d
Sergey Bova <odesseybova@gmail.com>
2023-07-25 11:18:19 +0300

    Add project files.

---
+++ Homework #1/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework__1
+{
+    public class Calculator
+    {
+        public double Add(double a, double b) { return a + b; }         //Додавання
+        public double Sub(double a, double b) { return a - b; }         //Віднімання
+        public double Mult(double a, double b) { return a * b; }       //Множення
+        public double Div(double a, double b) { return a / b; }         //Ділення
+        public double Perc(double a, double b) { return b / 100 * a; }      //Процент від числа. Знаходить процент "a" від числа "b"
+        public double ToPower(double a, double b) { return Math.Pow(a, b); }    //Возведення у степінь. Число "а" у степінь "b"
+    }
+}
---
+++ Homework #1/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Homework__1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculator();
+            double a, b, c = 0;
+            string oper;
+
+            Console.WriteLine("Enter first value");
+            a = double.Parse(Console.ReadLine());
+
+            while (true)    //Цикл який дозволяє користатись калькулятором на кшталт настільних електрониих калькуляторів, тобто можна продовжувати безкінечний ланцюжок операцій
+            {
+                Console.WriteLine("Enter operation");
+                oper = Console.ReadLine();
+                if (oper == "=")
+                {
+                    break;                      //Зупиняє цикл і надає можливість побачити результат
+                }
+                Console.WriteLine("Enter next value");
+                if (c != 0) a = c;
+                b = double.Parse(Console.ReadLine());
+                if (oper == "+")
+                {
+                   c =  calc.Add(a, b);
+                }
+                else if (oper == "-")
+                {
+                    c = calc.Sub(a, b);
+                }
+                else if (oper == "*")
+                {
+                    c = calc.Mult(a, b);
+                }
+                else if (oper == "/")
+                {
+                    c = calc.Div(a, b);
+                }
+                else if (oper == "%")
+                {
+                    c = calc.Perc(a, b);
+                }
+                else if (oper == "**")
+                {
+                    c = calc.ToPower(a, b);
+                }
+                else
+                {
+                    Console.WriteLine("Wrong operation symbol");
+                    break;
+                }
+            }
+            Console.WriteLine("Result  is " + c);
+        }
+    }
+
+}
