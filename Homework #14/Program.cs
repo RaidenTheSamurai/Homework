a77aaec1971f10396e6bf51a6817afea6829809d
Sergey Bova <odesseybova@gmail.com>
2023-11-08 11:13:13 +0200

    ok

---
+++ Homework #14/Program.cs
@@ -0,0 +1,34 @@
+﻿< !DOCTYPE html >
+< html lang = "en" >
+< head >
+    < meta charset = "UTF-8" >
+    < meta name = "viewport" content = "width=device-width, initial-scale=1.0" >
+    < title > My visitcard </ title >
+    < link rel = "stylesheet" href = "styles.css" >
+</ head >
+< body >
+    < header >
+        < h1 > Serhii Bova </ h1 >
+        < p > Beginner Unity3D developer</p>
+    </header>
+    <nav>
+        <ul>
+            <li><a href = "#about" > About me</a></li>
+           
+            <li><a href = "#contact" > Contact </ a ></ li >
+        </ ul >
+    </ nav >
+    < section id= "about" >
+        < h2 > About me</h2>
+        <p>Beginner Unity3D developer creating platformer mini-games</p>
+  </section>
+    <section id = "contact" >
+        < h2 > Contact </ h2 >
+        < p > Email: somemail @gmail.com </ p >
+        < p > Number: +380123456789 </ p >
+    </ section >
+    < footer >
+        < p > &copy; 2023 Serhii Bova</p>
+    </footer>
+</body>
+</html>
